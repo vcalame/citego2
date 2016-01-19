07d5df3400e7f25d66d776ecad25c568ac0b3689
Vincent Calame <vincent.calame@exemole.fr>
2016-01-19 18:05:27 +0100

    Ajout des liens info et desmo

--- public/static/js/modal.js
+++ public/static/js/modal.js
@@ -1,33 +1,41 @@
-$(function () {
+var Modal = function () {
     
+};
+
+Modal.load = function (href) {
+    var $modalBody = $("#commonModal").find(".modal-content");
+    $modalBody.load(href, function () {
+        $commonModal = $("#commonModal");
+        $commonModal.find("form").each(function () {
+            var $this = $(this);
+            var role = $this.data("role");
+            if (role === "term-remove") {
+                var id = $this.find("input[name='id']").val();
+                $this.ajaxForm({
+                    beforeSubmit: function () {
+                        var confirmation = confirm("Confirmez-vous la suppression du descripteur ?");
+                        if (!confirmation) {
+                            $("#commonModal").modal('hide');
+                            return false;
+                        }
+                    },
+                    success: function () {
+                        $("#descripteur_" + id).hide();
+                        $("#commonModal").modal('hide');
+                    }
+                });
+            }
+            
+        });
+        $commonModal.modal('show');
+    });
+};
+
+
+$(function () {
    var $modals = $("[data-link-type='modal']");
    $modals.click(function () {
-       var $modalBody = $("#commonModal").find(".modal-content");
-       $modalBody.load(this.href, function () {
-           $commonModal = $("#commonModal");
-           $commonModal.find("form").each(function () {
-               var $this = $(this);
-               var role = $this.data("role");
-               if (role === "term-remove") {
-                   var id = $this.find("input[name='id']").val();
-                    $this.ajaxForm({
-                        beforeSubmit: function () {
-                            var confirmation = confirm("Confirmez-vous la suppression du descripteur ?");
-                            if (!confirmation) {
-                                $("#commonModal").modal('hide');
-                                return false;
-                            }
-                        },
-                        success: function () {
-                            $("#descripteur_" + id).hide();
-                            $("#commonModal").modal('hide');
-                        }
-                    });
-               }
-               
-           });
-           $commonModal.modal('show');
-       });
+       Modal.load(this.href);
        return false;
    });
 });
